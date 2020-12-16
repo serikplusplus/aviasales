d826eda51ce56ed0c87b4f15f3591d1ac5b64cbd
”Sergey <serik.plus.plus@gmail.com>
2020-12-16 19:26:41 +0200

    release v1.0

--- main.js
+++ main.js
@@ -25,15 +25,11 @@ const getData = (url, callback) => {
     request.addEventListener('readystatechange', () => {
         if (request.readyState !== 4) return null;
 
-
         if (request.status === 200) {
             callback(request.response);
-        }
-        else if(request.status === 400)
-        {
+        } else if (request.status === 400) {
             alert("К сожалению билетов не найдено");
-        }
-        else {
+        } else {
             console.error(request.status);
         }
     });
@@ -41,7 +37,7 @@ const getData = (url, callback) => {
 };
 
 
-// TODO показ списка поиска  - город вылета
+// TODO показ списка поиска
 const showCities = (input, dropdown) => {
 
     dropdown.textContent = '';
@@ -54,7 +50,6 @@ const showCities = (input, dropdown) => {
             const fixItem = item.name.toLowerCase();
             return fixItem.startsWith(input.value.toLowerCase());
         });
-
 
         //создание списка из городов после фильтрации
         citiesFilter.forEach((item) => {
@@ -78,40 +73,81 @@ const handlerCity = (event, input, dropdown) => {
 };
 
 
+// TODO Получение количества пересадок
 const getChange = (data) => {
     if (data) {
-        return data === 1 ? "Одна пересадка" : `${data} пересадок`;
+        return data === 1 ? "Одна пересадка" : `Количество пересадок - ${data}`;
     } else return "Без пересадок";
 };
 
+// TODO Получение города по его коду
+const getCity = (data) => {
+    return cities.find(item => item.code === data).name;
+};
+
+// TODO Получение даты в правильном формате
+const getDate = (data) => {
+
+    return new Date(data).toLocaleString('ru', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    });
+};
+
+// TODO Получение нужной ссылки для билета
+const getLinkBy = (data) => {
+    let link = "https://www.aviasales.ru/search/";
+    link += data.origin;
+    const date = new Date(data.depart_date);
+    const day = date.getDate();
+    link += day < 10 ? '0' + day : day;
+    const month = date.getMonth() + 1;
+    link += month < 10 ? '0' + month : month;
+    link += data.destination + '1';
+    return link;
+};
+
+
+// TODO Формирование билета
 const createCard = (data) => {
 
-    const ticket = templateTicket.cloneNode(true);
-    if(data)
-    {
+    let ticket;
+    if (data) {
+        ticket = templateTicket.cloneNode(true);
         ticket.querySelector('.agent').textContent = data.gate;
-        ticket.querySelector('.price_ticket').textContent = "Купить за " + Math.floor(data.value/currency) + "₴";
-        ticket.querySelector('.name_from').textContent = data.origin;
-        ticket.querySelector('.date').textContent = data.depart_date;
+        ticket.querySelector('.price_ticket').textContent = "Купить за " + Math.floor(data.value / currency) + "₴";
+        ticket.querySelector('.button__buy').href = getLinkBy(data);
+        ticket.querySelector('.name_from').textContent = getCity(data.origin);
+        ticket.querySelector('.date').textContent = getDate(data.depart_date);
         ticket.querySelector('.changes').textContent = getChange(data.number_of_changes);
-        ticket.querySelector('.name_to').textContent = data.destination;
+        ticket.querySelector('.name_to').textContent = getCity(data.destination);
+    } else {
+        ticket = document.createElement('h3');
+        ticket.textContent = "К сожалению билетов нет";
     }
-    else ticket.querySelector('.cheapest-ticket-title').textContent = "К сожалению билетов нет";
     return ticket;
 };
 
-// TODO получение билетов по текущей дате
+// TODO Вывод билетов по текущей дате
 const renderCheapDate = (cheapTicket) => {
+    document.querySelector('.wrapper__ticket').style.display = 'block';
+    cheapestTicket.textContent = '';
     const ticket = createCard(cheapTicket[0]);
     cheapestTicket.append(ticket);
 };
 
-// TODO получение билетов на ближайшее время
+// TODO Вывод билетов на ближайшее время
 const renderCheapYear = (cheapTicket) => {
-    const tickets = cheapTicket.sort((a, b) => a.value - b.value).slice(0,10);
-    for (ticket of tickets){
-        otherCheapTickets.append(createCard(ticket));
-    }
+    document.querySelector('.block__ticket').style.display = 'block';
+    otherCheapTickets.textContent = '';
+
+    const tickets = cheapTicket.sort((a, b) => a.value - b.value).slice(0, 10);
+    if (tickets.length) {
+        for (let ticket of tickets) {
+            otherCheapTickets.append(createCard(ticket));
+        }
+    } else otherCheapTickets.append(createCard(tickets[0]));
 };
 
 // TODO получение билетов
@@ -123,34 +159,34 @@ const renderCheap = (data, when) => {
 };
 
 
-//Обработка ввода в input - Город вылета
+// TODO Обработка ввода в input - Город вылета
 inputCitiesFrom.addEventListener('input', () => {
     showCities(inputCitiesFrom, dropdownCitiesFrom);
 });
 
 
-//Обработка ввода в input - Город назначения
+// TODO Обработка ввода в input - Город назначения
 inputCitiesTo.addEventListener('input', () => {
     showCities(inputCitiesTo, dropdownCitiesTo);
 });
 
 
-//Обработка выбора города с выпадающего списка - dropdownCitiesFrom
+// TODO Обработка выбора города с выпадающего списка - dropdownCitiesFrom
 dropdownCitiesFrom.addEventListener('click', (event) => {
     handlerCity(event, inputCitiesFrom, dropdownCitiesFrom);
 });
 
 
-//Обработка выбора города с выпадающего списка - dropdownCitiesTo
+// TODO  Обработка выбора города с выпадающего списка - dropdownCitiesTo
 dropdownCitiesTo.addEventListener('click', (event) => {
     handlerCity(event, inputCitiesTo, dropdownCitiesTo);
 });
 
 
-//Обработка клика на кнопку поиска
+// TODO Обработка клика на кнопку поиска
 formSearch.addEventListener('submit', (event) => {
     event.preventDefault();
-    cheapestTicket.textContent = '';
+
     const formData = {
         from: cities.find((item) => inputCitiesFrom.value === item.name),
         to: cities.find((item) => inputCitiesTo.value === item.name),
@@ -165,13 +201,15 @@ formSearch.addEventListener('submit', (event) => {
     } else alert("Введите коректные названя городов");
 });
 
+// TODO Получение курса гривны
+getData(proxy + moneyApi, (data) => {
+    currency = JSON.parse(data);
+    currency = currency['uah'];
+});
 
+// TODO Получение списка городов
 getData(citiesApi, (data) => {
     cities = JSON.parse(data);
     cities.sort((a, b) => a.value - b.value);
 });
 
-getData( proxy + moneyApi, (data) => {
-    currency = JSON.parse(data);
-    currency = currency['uah'];
-});
